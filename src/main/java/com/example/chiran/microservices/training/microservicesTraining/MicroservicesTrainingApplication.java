0abda7fe11308a7650d9e122b0aa6e0c4ed87d3c
Chiran Hewawitharana <chiranh@zone24x7.com>
2020-11-21 07:56:30 +0530

    initial spring application commit

---
+++ src/main/java/com/example/chiran/microservices/training/microservicesTraining/MicroservicesTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.chiran.microservices.training.microservicesTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chiran/microservices/training/microservicesTraining/MicroservicesTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chiran.microservices.training.microservicesTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
